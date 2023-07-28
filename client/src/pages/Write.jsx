019e6e87c08c6207ed44ff486934949e405b0947
abhaysinghs772 <abhaysinghs772@gmail.com>
2023-07-28 11:56:39 +0530

    designing write page in progress...

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,16 @@
+import React from 'react';
+
+
 function Write() {
     return (
-        <div>
-            This is write page
+        <div className='add'>
+            <div className="content">
+                <input type="text" placeholder='title'/>
+            </div>
+            <div className="menu">
+                <div className="item">i1</div>
+                <div className="item">i2</div>
+            </div>
         </div>
     )
 }
